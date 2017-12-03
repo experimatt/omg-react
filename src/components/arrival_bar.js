fd5a0df0fb7bc4424099d3e076b5062d33a7ead0
Matt Decuir <decuirm@gmail.com>
2017-12-02 18:11:14 -0600

    More arrival bar formatting

--- src/components/arrival_bar.js
+++ src/components/arrival_bar.js
@@ -10,24 +10,29 @@ let directionMappings = {
 
 const ArrivalBar = (props) => {
   let arrowClass = `icon-omg-arrow-${directionMappings[props.RouteDirection]}`
+  let arrivalTime = moment(props.DepartureTime).format("h:mma")
 
   let minutesAway = moment(props.DepartureTime).diff(moment().startOf('minute'),'minutes')
   let chipClass = 'p20'
   if (minutesAway <= 10) {chipClass = 'p10'}
   if (minutesAway <= 5) {chipClass = 'p5'}
 
+  let arrivalText = ( props.Actual ? `${minutesAway} min` : arrivalTime )
+  let realTimeUnavailable = ( !props.Actual && minutesAway < 20 && <i title="Real-time data unavailable" className="fa fa-question-circle"></i> )
+  let descriptionText = ( props.Actual && arrivalTime )
+
   return (
     <div className={`arrival-bar ${chipClass}`}>
       <div className='arrival-info'>
-        <i className={arrowClass}></i>&nbsp;{props.Route}<br />
+        <i className={arrowClass}></i>&nbsp;{props.Route}{props.Terminal}<br />
         <span className='description-text'> { props.Description }</span>
       </div>
       <div className='arrival-time'>
-        { props.DepartureText }
-        &nbsp;
-        {!props.Actual && <i title="Real-time data unavailable" className="fa fa-question-circle"></i> }
+        { arrivalText }
         <br />
-        <div className='description-text'>{ `${minutesAway} min` }</div>
+        <div className='description-text'>
+          { descriptionText } { realTimeUnavailable }
+        </div>
       </div>
     </div>
   )
