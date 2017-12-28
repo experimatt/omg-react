4cf1e05e85670e901e86b5cfb68c9b14f2db0d47
Benjamin Rosas <BallPointPenguin@gmx.us>
2017-12-27 22:02:10 -0600

    move tests into top-level spec/ folder

--- spec/components/app.spec.js
+++ spec/components/app.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../../components/app';
+
+import App from '../../src/components/app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
